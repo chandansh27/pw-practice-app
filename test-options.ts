e379c322272d8e0d2e0f8d9b9a8a10135c271d99
First_Chandan Last_Sharma <sharma.chandan@hotmail.com>
2024-04-27 17:35:56 +0530

    Changes on 26th April24

--- test-options.ts
+++ test-options.ts
@@ -1,11 +1,31 @@
 import {test as base} from '@playwright/test'
+import { PageManager } from '../pw-practice-app/page-objects/pageManager'
 
 export type TestOptions= {
 
     globalsQaURL: string
+    formLayoutPage: String
+    pageManager: PageManager
+
 }
 
 export const test= base.extend<TestOptions>({
 
-    globalsQaURL: ['',{option: true}]
+    globalsQaURL: ['',{option: true}],
+
+    formLayoutPage: async({page}, use) =>{
+
+        await page.goto('/')
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+        await use('')
+        //console.log('Tear Down')
+    }, 
+
+    pageManager: async({page, formLayoutPage}, use)=> {
+        const pm= new PageManager(page)
+        await use(pm)
+
+        
+    }
 })
---
+++ tests/testwithFixures.spec.ts
@@ -0,0 +1,13 @@
+import {test} from '../test-options'
+import {faker} from '@faker-js/faker'
+
+test('parametrized methods', async({pageManager}) => {
+
+   const randomFullName= faker.person.fullName()
+   const randomEmail =`${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com` 
+
+   await pageManager.onFormLayoutPage().submitUsingTheGridFormWithCredentialsAndSelectOprion(process.env.USERNAME, process.env.PASSWORD, 'Option 2')
+   await pageManager.onFormLayoutPage().submInInlineWithNameEmailandCheckBox(randomFullName, randomEmail, true) 
+  
+})
+
